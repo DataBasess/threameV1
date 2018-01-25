217b287d87c24d60707bccb02e6b846d217a073c
DataBasess <anusondd@gmail.com>
2018-01-25 16:48:51 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import { Platform,AlertController, } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Slider } from '../pages/slider/slider';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = Slider;
+  @ViewChild('content') nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform, private statusBar: StatusBar, splashScreen: SplashScreen,public alertCtrl: AlertController) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.statusBar.backgroundColorByHexString('#512DA8');
   }
-}
 
+  gotosettings() {
+    this.nav.push('SettingsPage');
+  }
+  gotoplan() {
+    this.nav.push('ServiceplanPage');
+  }
+  gotowallet() {
+    this.nav.push('WalletPage');
+  }
+  gotoprofile(){
+    this.nav.push('ProfilePage');
+  }
+  audiocall(){
+    this.nav.push('AudiocallPage');
+  }
+  videocall(){
+    this.nav.push('VideocallPage');
+  }
+  nointernet(){
+    this.nav.push('NointernetPage');
+  }
+
+}
